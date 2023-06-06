aecf6c6762d874f72105b08c680a97480caa498b
Saysaw <g.o.developerue@gmail.com>
2023-06-06 16:50:17 +0500

    v0.2

--- Projects/js/tabs.js
+++ Projects/js/tabs.js
@@ -4,11 +4,16 @@ var technicalContent = document.querySelector('.technical__block');
 var designContent = document.getElementById('design-content');
 
 tabBtnOne.addEventListener('click', function() {
-  technicalContent.style.display = "block";
+  technicalContent.style.display = "flex"; // Измените 'block' на 'flex' здесь
   designContent.style.display = "none";
+  tabBtnOne.classList.add('active');
+  tabBtnTwo.classList.remove('active');
 });
 
 tabBtnTwo.addEventListener('click', function() {
   technicalContent.style.display = "none";
-  designContent.style.display = "block";
+  designContent.style.display = "block"; // Устанавливаем значение 'block' для 'designContent'
+  tabBtnOne.classList.remove('active');
+  tabBtnTwo.classList.add('active');
 });
+
---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.cookie = "cookieName=cookieValue; SameSite=None; Secure";
+document.cookie = "cookieName2=cookieValue2; SameSite=Strict";
+document.cookie = "cookieName3=cookieValue3; SameSite=Lax";
